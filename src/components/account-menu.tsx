808347c0eac37c765d95177aecdadeac90bfb8c1
BrentHoskins84 <brenthoskins84@gmail.com>
2025-12-28 10:16:43 -0800

    fix: update signOut function to return null error and adjust AccountMenu to redirect to home page after logout

--- src/components/account-menu.tsx
+++ src/components/account-menu.tsx
@@ -28,11 +28,11 @@ export function AccountMenu({ signOut }: { signOut: () => Promise<ActionResponse
         description: 'An error occurred while logging out. Please try again or contact support.',
       });
     } else {
-      router.refresh();
-
       toast({
         description: 'You have been logged out.',
       });
+
+      router.push('/');
     }
   }
 
--- src/features/auth/auth-actions.ts
+++ src/features/auth/auth-actions.ts
@@ -54,5 +54,5 @@ export async function signOut(): Promise<ActionResponse> {
     return { data: null, error: error };
   }
 
-  return redirect('/');
+  return { data: null, error: null };
 }
